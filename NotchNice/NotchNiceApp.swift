271a7fe545772f259e70de88a25aefd70bc5f6f6
bielgennaro <zirondigabriel@gmail.com>
2026-06-15 21:49:53 -0300

    Initial Commit

---
+++ NotchNice/NotchNiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotchNiceApp.swift
+//  NotchNice
+//
+//  Created by Biel on 15/06/26.
+//
+
+import SwiftUI
+
+@main
+struct NotchNiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
